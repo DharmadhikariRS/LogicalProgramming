11c348828fc416c8c577c35541f5dfa57f57d9d5
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-15 20:11:57 +0530

    [Rushikesh] UC4- Reverse number program

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Enter 1 for Fibonacci series program");
             Console.WriteLine("Enter 2 for Perfect Number program");
             Console.WriteLine("Enter 3 for Prime Number program");
+            Console.WriteLine("Enter 4 for Reverse Number program");
             int Check = Convert.ToInt32(Console.ReadLine());
             switch (Check)
             {
@@ -23,6 +24,10 @@
                 case 3:
                     PrimeNumbers primeNumbers = new PrimeNumbers();
                     primeNumbers.PrimeNumber();
+                    break;
+                case 4:
+                    ReverseNumber reverseNumber = new ReverseNumber();
+                    reverseNumber.ReverseNum();
                     break;
                 default:
                     Console.WriteLine("Wrong input");
---
+++ LogicalProgramming/ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class ReverseNumber
+    {
+        public void ReverseNum()
+        {
+            Console.WriteLine("Enter the number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int ReverseNum = 0;
+            int Remainder = 0;
+            while (Num > 0)
+            {
+                Remainder = Num % 10;
+                ReverseNum = ReverseNum * 10 + Remainder;
+                Num /= 10;
+            }
+            Console.WriteLine("Reverse number = " + ReverseNum);
+        }
+
+    }
+}
