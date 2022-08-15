a8976ba2a86ce1e76cb4a900ac6e5f437af34dbd
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-15 19:52:26 +0530

    [Rushikesh] UC1-Fibonacci series program

---
+++ LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public void FibonacciFn()
+        {
+            int firstNum = 0;
+            int secondNum = 1;
+            int i = 1;
+            int NextNum;
+            Console.WriteLine(" Enter number for  how many terms of series to be printed  ");
+
+            int Num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("First {0} Numbers of Fibonacci series Are ", Num);
+            Console.WriteLine();
+            Console.WriteLine(firstNum);
+            Console.WriteLine(secondNum);
+
+            while (i <= (Num - 2))
+            {
+                NextNum = secondNum + firstNum;
+                Console.WriteLine(NextNum);
+                firstNum = secondNum;
+                secondNum = NextNum;
+                i++;
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical programming");
+            Console.WriteLine("Enter 1 for Fibonacci series program");
+            int Check = Convert.ToInt32(Console.ReadLine());
+            switch (Check)
+            {
+                case 1:
+                    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                    fibonacciSeries.FibonacciFn();
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+
+            }
+        }
+    }
+}
